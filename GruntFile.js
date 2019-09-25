2ec22795b124c5e12df089e0262a19d45b6bf183
Renato Laranjo <renatolaranjo@gmail.com>
2019-09-24 21:40:31 -0300

    Readme atualizado

--- GruntFile.js
+++ GruntFile.js
@@ -1,6 +1,5 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-svg-sprite');
     grunt.initConfig({
         cssmin: {
             target: {
@@ -12,17 +11,6 @@ module.exports = function (grunt) {
                     ext: '.min.css'
                 }]
             }
-        },
-        svg_sprite: {
-            options: {
-                // Task-specific options go here.
-            },
-            images: {
-                expand: true,
-                cwd: 'images/colored',
-                src: ['**/*.svg'],
-                dest: 'images',
-            },
         }
     });
     grunt.registerTask('default', ['cssmin']);
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # br-icon
+Ícones de estados brasileiros com classes css.
+
+### Exemplo
+Os ícones possuem tamanho de 1 à 6, sem cor e sem borda.
+
+##### 1x
+```<i class="flag flag-mg flag-1x"></i>
+```
+
+##### 6x
+```<i class="flag flag-mg flag-6x"></i>
+```
+
+#### Grayscaled
+
+```
+<i class="flag flag-mg-bs flag-6x"></i>
+```
+
+#### Sem borda
+```
+<i class="flag flag-mg flag-3x flag-no-border"></i>
+```
